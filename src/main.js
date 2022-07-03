431c025de4c4c7a8da18fd02ed2e2ecec7ff2acf
jpliu <fortune_coder@foxmail.com>
2022-07-03 23:58:09 +0800

    feat: demo view through Hello World

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-
+import VPreview from "./components/v-preview";
 Vue.config.productionTip = false;
+
+Vue.use(VPreview);
 
 new Vue({
   render: (h) => h(App),
